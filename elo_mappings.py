12f81d86aa0acb892d4967b14bcfc10d272b544f
Goldenbeasty <goldenbeaster@gmail.com>
2023-08-01 01:57:59 +0300

    Working towards usability

--- elo_mappings.py
+++ elo_mappings.py
@@ -3,16 +3,19 @@ elo_map = {
     "subs": 10,
     "subgift": 20,
     "normal_message_token": 2,
-    "timeouts": -10
+    "timeouts": -10, # positive for testing
+    "channelpointredeems": 10
 
 }
 
+# there is a better way to do it procedually, I will persue that
 elo_re = {
-    "message" : [r"^\[\d{2}:\d{2}:\d{2}\]  \w*: (.*)$", r"^\[\d{2}:\d{2}:\d{2}\] (\S* \S*):.*$"],
+    "message" : [r"^\[\d{2}:\d{2}:\d{2}\]  (\w*): (.*)$", r"^\[\d{2}:\d{2}:\d{2}\] (\S* \S*): (.*)$"],
     "anygiftersinthechat": [r"^\[\d{2}:\d{2}:\d{2}\] (\S*) gifted a Tier [123] sub to \S*$"],
-    "subs": [r"^\[\d{2}:\d{2}:\d{2}\] (\S*) subscribed (?:with Prime|at Tier [123]). They've subscribed for \d* months(?:!|, currently on a \d* month streak!)$", r"^\[\d{2}:\d{2}:\d{2}\] (\S*) subscribed with Prime.$"],
-    "sysmessages": [r"^# Start logging at \d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} \S*$", r"^\[\d{2}:\d{2}:\d{2}] connected$", r"^\[\d{2}:\d{2}:\d{2}\] disconnected$", r"\[\d{2}:\d{2}:\d{2}\] (\S*) is live!$", r"\[\d{2}:\d{2}:\d{2}\] \S* gifted a Tier [123] sub to \S*! They have given \d* Gift Subs in the channel!$"],
-    "timeouts": [r"^\[\d{2}:\d{2}:\d{2}\] (\S*) has been timed out for \S*\. $"]
+    "subs": [r"^\[\d{2}:\d{2}:\d{2}\] (\S*) subscribed (?:with Prime|at Tier [123]). They've subscribed for \d* months(?:!|, currently on a \d* month streak!)$", r"^\[\d{2}:\d{2}:\d{2}\] (\S*) subscribed with Prime.$", r"^\[\d{2}:\d{2}:\d{2}\] (\S*) subscribed at Tier [123].$"],
+    "sysmessages": [r"^# Start logging at \d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} \S*$", r"^\[\d{2}:\d{2}:\d{2}] connected$", r"^\[\d{2}:\d{2}:\d{2}\] disconnected$", r"\[\d{2}:\d{2}:\d{2}\] (\S*) is live!$", r"\[\d{2}:\d{2}:\d{2}\] \S* gifted a Tier [123] sub to \S*! They have given \d* Gift Subs in the channel!$", r"^\[\d{2}:\d{2}:\d{2}\] [MTWFS][a-z]*, \d [JFMASOND][a-z]* \d{4}$", r"^\[\d{2}:\d{2}:\d{2}\] Server connection timed out, reconnecting$", r"^\[\d{2}:\d{2}:\d{2}\] (?:Twitch subscriber|BetterTTV channel|7TV channel) emotes reloaded.$", r"^\[\d{2}:\d{2}:\d{2}\] \S* is continuing the Gift Sub they got from \S*!$", r"^\[\d{2}:\d{2}:\d{2}\] (?:\S*) is now offline.$", r"^\[\d{2}:\d{2}:\d{2}\] You were timed out for .*$", r"^\[\d{2}:\d{2}:\d{2}\] An anonymous user gifted a Tier [123] sub to \S*$", r"^\[\d{2}:\d{2}:\d{2}\] \S* added 7TV emote \S*.$", r"^\[\d{2}:\d{2}:\d{2}\] Failed to fetch (?:BetterTTV|FrankerFaceZ|7TV) channel emotes. \(unknown error\)$", r"^\[\d{2}:\d{2}:\d{2}\] This room is now in slow mode. You may send messages every \d* \S*.$", r"^\[\d{2}:\d{2}:\d{2}\] Failed to load channel badges - OAuth token is missing$", r"^\[\d{2}:\d{2}:\d{2}\] \S* is paying forward the Gift they got from \S* to (?:the community|\S*)!$"],
+    "timeouts": [r"^\[\d{2}:\d{2}:\d{2}\] (\S*) has been timed out for (?:\S*|\S* \S*)\.$", r"^\[\d{2}:\d{2}:\d{2}\] (\S*) has been permanently banned.$"],
+    "channelredeems": [r"^\[\d{2}:\d{2}:\d{2}\] (\S*) redeemed .*$"]
 }
 
 
--- main.py
+++ main.py
@@ -7,25 +7,31 @@ import elo_mappings
 
 chatpath = "~/.local/share/chatterino/Logs/Twitch/Channels/vedal987/"
 LOOKAROUND_LEN = 15
+DEBUG = True
 
 chatpath = os.path.expanduser(chatpath)
 
-def compile_re():
-    re_messages_comp = []
-    for expression in elo_mappings.elo_re["message"]:
-        re_messages_comp.append(re.compile(expression))
-    re_anygiftersinchat_comp = []
-    for expression in elo_mappings.elo_re["anygiftersinthechat"]:
-        re_anygiftersinchat_comp.append(re.compile(expression))
-    re_subs_comp = []
-    for expression in elo_mappings.elo_re["subs"]:
-        re_subs_comp.append(re.compile(expression))
-    re_sysmessages_comp = []
-    for expression in elo_mappings.elo_re["sysmessages"]:
-        re_sysmessages_comp.append(re.compile(expression))
-    re_timeouts_comp = []
-    for expression in elo_mappings.elo_re["timeouts"]:
-        re_timeouts_comp.append(re.compile(expression))
+if DEBUG:
+    rejectslist = []
+
+re_messages_comp = []
+for expression in elo_mappings.elo_re["message"]:
+    re_messages_comp.append(re.compile(expression))
+re_anygiftersinchat_comp = []
+for expression in elo_mappings.elo_re["anygiftersinthechat"]:
+    re_anygiftersinchat_comp.append(re.compile(expression))
+re_subs_comp = []
+for expression in elo_mappings.elo_re["subs"]:
+    re_subs_comp.append(re.compile(expression))
+re_sysmessages_comp = []
+for expression in elo_mappings.elo_re["sysmessages"]:
+    re_sysmessages_comp.append(re.compile(expression))
+re_timeouts_comp = []
+for expression in elo_mappings.elo_re["timeouts"]:
+    re_timeouts_comp.append(re.compile(expression))
+re_channelredeems_comp = []
+for expression in elo_mappings.elo_re["channelredeems"]:
+    re_channelredeems_comp.append(re.compile(expression))
 
 def repass_getuser(message: str, pattern: str) -> str:
     res_re = re.match(pattern, message)
@@ -33,6 +39,7 @@ def repass_getuser(message: str, pattern: str) -> str:
 
 
 def grade_text(message: str, context_ptr: int) -> int:
+    return int(elo_mappings.elo_map["normal_message_token"] * len(message) / 10) + 1
     pass
 
 def grade_elo(message: str, context_ptr: int) -> Union[None, Tuple[str, int]]:
@@ -44,11 +51,11 @@ def grade_elo(message: str, context_ptr: int) -> Union[None, Tuple[str, int]]:
         res_re = re.match(expression, message)
         if res_re:
             user_affected = res_re.group(1)
-            elo_delta += grade_text(message, context_ptr)
+            elo_delta += grade_text(res_re.group(2), context_ptr)
             break
 
     ### anysubgiftersinchat
-    for expression in re_anysubgiftersinchat_comp:
+    for expression in re_anygiftersinchat_comp:
         res_re = re.match(expression, message)
         if res_re:
             user_affected = res_re.group(1)
@@ -69,6 +76,14 @@ def grade_elo(message: str, context_ptr: int) -> Union[None, Tuple[str, int]]:
         if res_re:
             return None
 
+    ### channelredeems
+    for expression in re_channelredeems_comp:
+        res_re = re.match(expression, message)
+        if res_re:
+            user_affected = res_re.group(1)
+            elo_delta += elo_mappings.elo_map["channelpointredeems"]
+            break
+
     ### timeouts
     for expression in re_timeouts_comp:
         res_re = re.match(expression, message)
@@ -78,7 +93,9 @@ def grade_elo(message: str, context_ptr: int) -> Union[None, Tuple[str, int]]:
             break
 
     if elo_delta == 0:
-        print(f"Nodelta: {message}")
+        #print(f"Nodelta: {message}")
+        if DEBUG:
+            rejectslist.append(message + "\n")
         return None
     return user_affected, elo_delta
 
@@ -87,7 +104,6 @@ def main():
     chatlist = os.listdir(chatpath)
     chatlog = []
     elolist = {}
-    compile_re()
     for file in chatlist:
         with open(os.path.join(chatpath, file)) as f:
             this = f.readlines()
@@ -97,6 +113,10 @@ def main():
     ## time to assign elos
     for index, message in enumerate(chatlog):
         grade_elo(message, index)
+
+    if DEBUG:
+        with open("./debug_rejects.txt", "w") as f:
+            f.writelines(rejectslist)
 
 if __name__ == "__main__":
     main()
