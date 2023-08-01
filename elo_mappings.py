e47b4eabc207f4103d22553083486b6e5fd294ef
Goldenbeasty <goldenbeaster@gmail.com>
2023-08-01 13:19:49 +0300

    Remade mappings, added optimizations

--- elo_mappings.py
+++ elo_mappings.py
@@ -1,21 +1,37 @@
-elo_map = {
-
-    "subs": 10,
-    "subgift": 20,
-    "normal_message_token": 2,
-    "timeouts": -10, 
-    "channelpointredeems": 10
-
-}
-
-# there is a better way to do it procedually, I will persue that
-elo_re = {
-    "message" : [r"^\[\d{2}:\d{2}:\d{2}\]  (\w*): (.*)$", r"^\[\d{2}:\d{2}:\d{2}\] (\S* \S*): (.*)$"],
-    "anygiftersinthechat": [r"^\[\d{2}:\d{2}:\d{2}\] (\S*) gifted a Tier [123] sub to \S*$"],
-    "subs": [r"^\[\d{2}:\d{2}:\d{2}\] (\S*) subscribed (?:with Prime|at Tier [123]). They've subscribed for \d* months(?:!|, currently on a \d* month streak!)$", r"^\[\d{2}:\d{2}:\d{2}\] (\S*) subscribed with Prime.$", r"^\[\d{2}:\d{2}:\d{2}\] (\S*) subscribed at Tier [123].$"],
-    "sysmessages": [r"^# Start logging at \d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} \S*$", r"^\[\d{2}:\d{2}:\d{2}] connected$", r"^\[\d{2}:\d{2}:\d{2}\] disconnected$", r"\[\d{2}:\d{2}:\d{2}\] (\S*) is live!$", r"\[\d{2}:\d{2}:\d{2}\] \S* gifted a Tier [123] sub to \S*! They have given \d* Gift Subs in the channel!$", r"^\[\d{2}:\d{2}:\d{2}\] [MTWFS][a-z]*, \d [JFMASOND][a-z]* \d{4}$", r"^\[\d{2}:\d{2}:\d{2}\] Server connection timed out, reconnecting$", r"^\[\d{2}:\d{2}:\d{2}\] (?:Twitch subscriber|BetterTTV channel|7TV channel) emotes reloaded.$", r"^\[\d{2}:\d{2}:\d{2}\] \S* is continuing the Gift Sub they got from \S*!$", r"^\[\d{2}:\d{2}:\d{2}\] (?:\S*) is now offline.$", r"^\[\d{2}:\d{2}:\d{2}\] You were timed out for .*$", r"^\[\d{2}:\d{2}:\d{2}\] An anonymous user gifted a Tier [123] sub to \S*$", r"^\[\d{2}:\d{2}:\d{2}\] \S* added 7TV emote \S*.$", r"^\[\d{2}:\d{2}:\d{2}\] Failed to fetch (?:BetterTTV|FrankerFaceZ|7TV) channel emotes. \(unknown error\)$", r"^\[\d{2}:\d{2}:\d{2}\] This room is now in slow mode. You may send messages every \d* \S*.$", r"^\[\d{2}:\d{2}:\d{2}\] Failed to load channel badges - OAuth token is missing$", r"^\[\d{2}:\d{2}:\d{2}\] \S* is paying forward the Gift they got from \S* to (?:the community|\S*)!$"],
-    "timeouts": [r"^\[\d{2}:\d{2}:\d{2}\] (\S*) has been timed out for (?:\S*|\S* \S*)\.$", r"^\[\d{2}:\d{2}:\d{2}\] (\S*) has been permanently banned.$"],
-    "channelredeems": [r"^\[\d{2}:\d{2}:\d{2}\] (\S*) redeemed .*$"]
-}
-
-
+elo_mappings = [
+    {
+        "name": "stdmessage token",
+        "elo_award": 2,
+        "re": [r"^\[\d{2}:\d{2}:\d{2}\]  (\w*): (.*)$", r"^\[\d{2}:\d{2}:\d{2}\] (\S* \S*): (.*)$"]
+    },
+    {
+        "name": "Gifted sub",
+        "elo_award": 20,
+        "re": [r"^\[\d{2}:\d{2}:\d{2}\] (\S*) gifted a Tier [123] sub to \S*$"]
+    },
+    {
+        "name": "std sub",
+        "elo_award": 10,
+        "re": [r"^\[\d{2}:\d{2}:\d{2}\] (\S*) subscribed (?:with Prime|at Tier [123]). They've subscribed for \d* months(?:!|, currently on a \d* month streak!)$", r"^\[\d{2}:\d{2}:\d{2}\] (\S*) subscribed with Prime.$", r"^\[\d{2}:\d{2}:\d{2}\] (\S*) subscribed at Tier [123].$"]
+    },
+    {
+        "name": "system message",
+        "elo_award": None,
+        "re": [r"^# Start logging at \d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} \S*$", r"^\[\d{2}:\d{2}:\d{2}] connected$", r"^\[\d{2}:\d{2}:\d{2}\] disconnected$", r"\[\d{2}:\d{2}:\d{2}\] (\S*) is live!$", r"\[\d{2}:\d{2}:\d{2}\] \S* gifted a Tier [123] sub to \S*! They have given \d* Gift Subs in the channel!$", r"^\[\d{2}:\d{2}:\d{2}\] [MTWFS][a-z]*, \d [JFMASOND][a-z]* \d{4}$", r"^\[\d{2}:\d{2}:\d{2}\] Server connection timed out, reconnecting$", r"^\[\d{2}:\d{2}:\d{2}\] (?:Twitch subscriber|BetterTTV channel|7TV channel) emotes reloaded.$", r"^\[\d{2}:\d{2}:\d{2}\] \S* is continuing the Gift Sub they got from \S*!$", r"^\[\d{2}:\d{2}:\d{2}\] (?:\S*) is now offline.$", r"^\[\d{2}:\d{2}:\d{2}\] You were timed out for .*$", r"^\[\d{2}:\d{2}:\d{2}\] An anonymous user gifted a Tier [123] sub to \S*$", r"^\[\d{2}:\d{2}:\d{2}\] \S* added 7TV emote \S*.$", r"^\[\d{2}:\d{2}:\d{2}\] Failed to fetch (?:BetterTTV|FrankerFaceZ|7TV) channel emotes. \(unknown error\)$", r"^\[\d{2}:\d{2}:\d{2}\] This room is now in slow mode. You may send messages every \d* \S*.$", r"^\[\d{2}:\d{2}:\d{2}\] Failed to load channel badges - OAuth token is missing$", r"^\[\d{2}:\d{2}:\d{2}\] \S* is paying forward the Gift they got from \S* to (?:the community|\S*)!$"]
+    },
+    {
+        "name": "timeout",
+        "elo_award": -10,
+        "re": [r"^\[\d{2}:\d{2}:\d{2}\] (\S*) has been timed out for (?:\S*|\S* \S*)\.$"]
+    },
+    {
+        "name": "permaban",
+        "elo_award": -1000,
+        "re": [r"^\[\d{2}:\d{2}:\d{2}\] (\S*) has been permanently banned.$"]
+    },
+    {
+        "name": "channelredeem",
+        "elo_award": 10,
+        "re": [r"^\[\d{2}:\d{2}:\d{2}\] (\S*) redeemed .*$"]
+    }
+]
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 import re
 from typing import Tuple, Union
 
-import elo_mappings
+from elo_mappings import elo_mappings
 
 chatpath = "~/.local/share/chatterino/Logs/Twitch/Channels/vedal987/"
 LOOKAROUND_LEN = 15
@@ -13,25 +13,6 @@ chatpath = os.path.expanduser(chatpath)
 
 if DEBUG:
     rejectslist = []
-
-re_messages_comp = []
-for expression in elo_mappings.elo_re["message"]:
-    re_messages_comp.append(re.compile(expression))
-re_anygiftersinchat_comp = []
-for expression in elo_mappings.elo_re["anygiftersinthechat"]:
-    re_anygiftersinchat_comp.append(re.compile(expression))
-re_subs_comp = []
-for expression in elo_mappings.elo_re["subs"]:
-    re_subs_comp.append(re.compile(expression))
-re_sysmessages_comp = []
-for expression in elo_mappings.elo_re["sysmessages"]:
-    re_sysmessages_comp.append(re.compile(expression))
-re_timeouts_comp = []
-for expression in elo_mappings.elo_re["timeouts"]:
-    re_timeouts_comp.append(re.compile(expression))
-re_channelredeems_comp = []
-for expression in elo_mappings.elo_re["channelredeems"]:
-    re_channelredeems_comp.append(re.compile(expression))
 
 def repass_getuser(message: str, pattern: str) -> str:
     res_re = re.match(pattern, message)
@@ -45,58 +26,27 @@ def grade_text(message: str, context_ptr: int) -> int:
 def grade_elo(message: str, context_ptr: int) -> Union[None, Tuple[str, int]]:
     elo_delta = 0
     user_affected = ""
+    re_matched = False
 
-    ### std message
-    for expression in re_messages_comp:
-        res_re = re.match(expression, message)
-        if res_re:
-            user_affected = res_re.group(1)
-            elo_delta += grade_text(res_re.group(2), context_ptr)
-            break
+    for category in elo_mappings:
+        for expression in category["re"]:
+            re_res = re.match(expression, message)
+            if re_res:
+                re_matched = True
+                if category["elo_award"]:
+                    elo_delta += category["elo_award"]
+                    user_affected = re_res.group(1)
+                    return user_affected, elo_delta
+                else:
+                    return None
 
-    ### anysubgiftersinchat
-    for expression in re_anygiftersinchat_comp:
-        res_re = re.match(expression, message)
-        if res_re:
-            user_affected = res_re.group(1)
-            elo_delta += elo_mappings.elo_map["subgift"]
-            break
 
-    ### subs
-    for expression in re_subs_comp:
-        res_re = re.match(expression, message)
-        if res_re:
-            user_affected = res_re.group(1)
-            elo_delta += elo_mappings.elo_map["subs"]
-            break
-
-    ### sysmessages
-    for expression in re_sysmessages_comp:
-        res_re = re.match(expression, message)
-        if res_re:
-            return None
-
-    ### channelredeems
-    for expression in re_channelredeems_comp:
-        res_re = re.match(expression, message)
-        if res_re:
-            user_affected = res_re.group(1)
-            elo_delta += elo_mappings.elo_map["channelpointredeems"]
-            break
-
-    ### timeouts
-    for expression in re_timeouts_comp:
-        res_re = re.match(expression, message)
-        if res_re:
-            user_affected = res_re.group(1)
-            elo_delta += elo_mappings.elo_map["timeouts"]
-            break
-
-    if elo_delta == 0:
+    if not re_matched:
         #print(f"Nodelta: {message}")
         if DEBUG:
             rejectslist.append(message + "\n")
         return None
+    raise ValueError(f"matched, but not returned value: {message}")
     return user_affected, elo_delta
 
 def main():
