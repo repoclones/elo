bc08b8187b060a1aad2686b7fc36957c83a67f40
Goldenbeasty <goldenbeaster@gmail.com>
2023-08-05 16:15:57 +0300

    Cleaned up some variables

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from typing import Tuple, Union, List
 from elo_mappings import elo_mappings
 
 chatpath = "~/.local/share/chatterino/Logs/Twitch/Channels/vedal987/"
+chatpath = "./debug_input_data/"
 LOOKAROUND_LEN = 15
 DEBUG = True
 
@@ -14,7 +15,7 @@ chatpath = os.path.expanduser(chatpath)
 with open("./assets/emotelist.txt") as f:
     emotelist = f.readlines()
 
-emotelist = [s.strip() for s in emotelist]
+emotelist = [s.strip() for s in emotelist] # all twitch and 7TV, etc. emotes that can be in the messages
 
 if DEBUG:
     rejectslist = []
@@ -23,36 +24,32 @@ def filter_out_strings(original_string: str, strings_to_filter: List[str]) -> st
     return ''.join([word for word in original_string.split() if word not in strings_to_filter])
 
 def grade_text(message: str, context_ptr: int) -> int:
+    # return elo delta
+
     message = filter_out_strings(message, emotelist)
     return int(elo_mappings[0]["elo_award"] * len(message) / 10) + 1
 
 def grade_elo(message: str, context_ptr: int) -> Union[None, Tuple[str, int]]:
-    elo_delta = 0
-    user_affected = ""
-    re_matched = False
-
     for category in elo_mappings:
         for expression in category["re"]:
             re_res = re.match(expression, message)
             if re_res:
-                re_matched = True
                 if category["elo_award"]:
                     user_affected = re_res.group(1)
                     if category["name"] == "stdmessage token":
-                        elo_delta += grade_text(re_res.group(2), context_ptr)
+                        elo_delta = grade_text(re_res.group(2), context_ptr)
                     else:
-                        elo_delta += category["elo_award"]
+                        elo_delta = category["elo_award"]
                     return user_affected, elo_delta
                 else:
                     return None
 
 
-    if not re_matched:
-        print(f"WARNING: Unhandled log: {message}")
-        if DEBUG:
-            rejectslist.append(message + "\n")
-        return None
-    raise ValueError(f"matched, but not returned value: {message}")
+    #in case it doens't match
+    print(f"WARNING: Unhandled log: {message}")
+    if DEBUG:
+        rejectslist.append(message + "\n")
+    return None
 
 def main():
     ### Load the chat logs into memory
@@ -75,6 +72,7 @@ def main():
             else:
                 elolist[user] = elo_delta
 
+    ### Write out the new values
     json.dump(elolist, open("debug_elolist.json", "w"), indent=4)
 
     if DEBUG:
