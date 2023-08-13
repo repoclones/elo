251fbb5d4f84113b94f0c702db3ae8b63bc21989
Goldenbeasty <goldenbeaster@gmail.com>
2023-08-13 16:55:17 +0300

    Update the generation command

--- website/create_website.sh
+++ website/create_website.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 
-INPUT_FOLDER=$"src"
-OUTPUT_FOLDER=$"neuroelo_web"
+INPUT_FOLDER="src"
+OUTPUT_FOLDER="neuroelo_web"
+
+RUN_API_GENERATOR=true #the python scripts
+
+# Loop through the arguments
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -n)
+            RUN_API_GENERATOR=false
+            shift
+            ;;
+        *)
+            echo "Unknown argument: $1"
+            exit 1
+            ;;
+    esac
+done
 
 cp $INPUT_FOLDER/* $OUTPUT_FOLDER/
 
-#cp $INPUT_FOLDER/index.html $OUTPUT_FOLDER/
-tailwindcss -i $INPUT_FOLDER/styles.css -o $OUTPUT_FOLDER/styles.css
+tailwindcss -i $INPUT_FOLDER/styles.css -o $OUTPUT_FOLDER/styles.css -m
 
-#cp $INPUT_FOLDER/ $OUTPUT_FOLDER/
+if [ "$RUN_API_GENERATOR" == true ]; then
+	../venv/bin/python generate_api.py
+fi
